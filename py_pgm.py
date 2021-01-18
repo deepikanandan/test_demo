a0feb2e7537dfd7fbf9cf70128024e11b804e350
deepikanandan <deepika.nandan123@gmail.com>
2021-01-19 00:48:58 +0530

    edited

--- py_pgm.py
+++ py_pgm.py
@@ -1,18 +1,5 @@
 import random
-
-x = "y"
-
-while x == "y":
 	
-	num = random.randint(1,4) 
-	if num == 1:
-		print("1")
-	if num == 2:
-		print("2")
-	if num == 3:
-		print("3")
-	if num == 4:
-		print("4")
+num = random.randint(1,4) 
 
-	x = input("enter y to repeat else press n")
-	print("\n")
+print(num)
